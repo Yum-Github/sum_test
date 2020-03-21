72b1d5fbb88b0b225d3271286604c54a771bf534
yum <men1999@naver.com>
2020-03-22 05:09:13 +0900

    Add sum modules

--- sum.cpp
+++ sum.cpp
@@ -5,10 +5,7 @@ int sum(int n){
 	for (int i = 1; i <= n; i++)
 		res += i;
 	return res;
-}#include "sum.h"
+}
 
-int sum(int n){
-	int res = 0;
-	for (int i = 1; i <= n; i++)
-		res += i;
-	return res;
+
+
